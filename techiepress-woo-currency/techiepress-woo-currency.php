1161a6a6b2b8b5b7c7b77624c0db7410374fe8af
Laurence Bahiirwa <laurence@mediauganda.com>
2020-09-01 13:11:44 +0300

    Update techiepress-woo-currency.php

--- techiepress-woo-currency/techiepress-woo-currency.php
+++ techiepress-woo-currency/techiepress-woo-currency.php
@@ -11,17 +11,23 @@
  * text-domain: ocotw
 */
 
+/**
+ * Add new Country/Economy currency.
+ */
 add_filter( 'woocommerce_currencies', 'techiepress_add_world_currencies' );
-add_filter( 'woocommerce_currency_symbol', 'techiepress_add_world_currencies_symbol', 10, 2 );
 
 function techiepress_add_world_currencies( $currencies ) {
 
-	// Add out currency of Abkhazia
+	// Add currency of Abkhazia
 	$currencies['ARUB'] = __( 'Abkahzia Kopek', 'ocotw' );
 
 	return $currencies;
 }
 
+/**
+ * Add new Country/Economy currency code.
+ */
+add_filter( 'woocommerce_currency_symbol', 'techiepress_add_world_currencies_symbol', 10, 2 );
 
 function techiepress_add_world_currencies_symbol( $currency_symbol, $currency ) {
 	switch ( $currency ) {
@@ -30,4 +36,26 @@ function techiepress_add_world_currencies_symbol( $currency_symbol, $currency ) 
 		break;
 	}
 	return $currency_symbol;
+}
+
+/**
+ * Change an exisiting currency
+ *
+ * Go to https://github.com/woocommerce/woocommerce/blob/master/includes/wc-core-functions.php
+ * In the get_woocommerce_currencies() to find your currency code.
+ */
+add_filter( 'woocommerce_currency_symbol', 'techiepress_change_woocommerce_currency_symbol', 10, 2 );
+
+function techiepress_change_woocommerce_currency_symbol( $currency_symbol, $currency ) {
+    
+    switch( $currency ) {
+		    
+	// Insert currency code from the get_woocommerce_currencies().
+        case 'AED' : $currency_symbol = 'AED';
+        break;
+		    
+    }
+    
+    return $currency_symbol;
+    
 }
